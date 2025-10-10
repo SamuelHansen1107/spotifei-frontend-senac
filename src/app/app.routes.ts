7cb72907e98e5ff18c95c5b242e49a3c8c904b1d
Marcos <marcosantonio240604@gmail.com>
2025-10-10 19:51:31 -0300

    adicionando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent,
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path:"cadastro",
+        component: CadastroComponent
+    },
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+    }
+];
