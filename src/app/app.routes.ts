a83e8f08f4c742869af679fccf6694767a9f8a65
Samuel Hansen <samuelhansen07@hotmail.com>
2025-10-13 19:38:23 -0300

    att

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "cadastro",
+        component: CadastroComponent
+    },
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+    }
+];
