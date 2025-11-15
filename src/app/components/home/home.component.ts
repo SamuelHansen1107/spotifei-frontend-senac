fa18df67099047e15627665ecfb0ccc8423bb910
Samuel Hansen <samuelhansen07@hotmail.com>
2025-11-15 15:00:51 -0300

    Integração com backend

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from "@angular/router";
 import { CardMusicaComponent } from './card-musica/card-musica.component';
 import { Musica } from './card-musica/musica';
 import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -13,27 +14,9 @@ import { NgFor } from '@angular/common';
 })
 export class HomeComponent 
 {
-  musicas : Musica[] =
-  [
-    {
-      titulo: "Camisa 10",
-      artista: "Turma do Pagode",
-    },
-    {
-      titulo: "Faroeste Caboclo",
-      artista: "Legião Urbana",
-    },
-    {
-      titulo: "Black",
-      artista: "Pearl Jam",
-    },
-    {
-      titulo: "Segundo Sol",
-      artista: "Cássia Eller",
-    },
-    {
-      titulo: "Sozinho",
-      artista: "Caetano Veloso",
-    },
-  ];
+  musicas : Musica [] = [];
+  constructor (private httpClient : HttpClient) {}
+  ngOnInit(): void{
+    this.httpClient.get<Musica[]>('/api/musicas').subscribe((musicas) => this.musicas = musicas);
+  }
 }
